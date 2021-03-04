a10aa98c7e054436757f542f55b9aac8e56172ec
Julio Fajardo <jfajardoh5@gmail.com>
2021-03-04 13:10:10 -0500

    Began working on index page style.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,24 @@
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
+        <style>
+            body {
+                background-color: #f1f1f1;
+                background-image: url("data:image/svg+xml,%3Csvg width='42' height='44' viewBox='0 0 42 44' xmlns='http://www.w3.org/2000/svg'%3E%3Cg id='Page-1' fill='none' fill-rule='evenodd'%3E%3Cg id='brick-wall' fill='%23a2a2a2' fill-opacity='0.21'%3E%3Cpath d='M0 0h42v44H0V0zm1 1h40v20H1V1zM0 23h20v20H0V23zm22 0h20v20H22V23z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+            }
+            .ribbon {
+                position: absolute;
+                top: 0;
+                right: 0;
+                width: 150px;
+                height: 22px;
+                margin-right: -50px;
+                margin-top: 15px;
+                -ms-transform: rotate(45deg); /* IE 9 */
+                -webkit-transform: rotate(45deg); /* Safari prior 9.0 */
+                transform: rotate(45deg); /* Standard syntax */
+            }
+        </style>
     </head>
     <body>
         <div class="font-sans text-gray-900 antialiased">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\PlayingCard;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['cards' => PlayingCard::get()]);
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
