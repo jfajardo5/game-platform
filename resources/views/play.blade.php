fad9073888e2cc7c1e5e580589d7130f0bdf2508
Julio Fajardo <jfajardoh5@gmail.com>
2021-03-04 23:04:55 -0500

    Fleshing out gameplay. Win conditions need to be revised.

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,43 +1,51 @@
 <x-app-layout>
 	<div class="py-12">
         <div x-data="game()" class="mx-auto overflow-hidden bg-white shadow-xl max-w-7xl sm:px-6 lg:px-8 sm:rounded-lg">
-            <div class="flex flex-row flex-wrap justify-center ">
-                <button @click="playerHits();gameLogic()">Player Hit</button>
-                <button @click="playerSits();gameLogic()">Player Sit</button>
-            </div>
-            <div class="flex flex-col">
-            	<h2>Dealer Cards:</h2>
-                <div class="flex flex-row items-center space-around">
+        	<div class="flex flex-row justify-between p-4">
+				<h2>Dealer: <span x-text="dealerScore"></span> pts</h2>
+            	<h2>Player: <span x-text="playerScore"></span> pts</h2>
+        	</div>
+            <div class="flex flex-col p-8 h-80">
+                <div class="flex flex-row items-center justify-center space-around">
                 	<template x-for="card in dealerCards" :key="card">
-						<div class="flex flex-col items-center justify-center p-4 text-center">
-							<label :text="card.name"></label>
-							<image :src="card.image_url" class="w-40 p-2">
+						<div class="flex flex-col items-center justify-center p-4 text-center animate__animated animate__slower animate__fadeIn">
+							<label x-text="card.name + ' of ' + card.group"></label>
+							<image :src="card.image_url" class="w-32 p-2">
 						</div>
                 	</template>
                 </div>
             </div>
-			<div class="flex flex-col">
-            	<h2>Player Cards:</h2>
-                <div class="flex flex-row items-center space-around">
+			<div class="flex flex-col p-8 h-80">
+                <div class="flex flex-row items-center justify-center space-around">
                 	<template x-for="card in playerCards" :key="card">
-						<div class="flex flex-col items-center justify-center p-4 text-center">
-							<label :text="card.name"></label>
-							<image :src="card.image_url" class="w-40 p-2">
+						<div class="flex flex-col items-center justify-center p-4 text-center animate__animated animate__slower animate__fadeIn">
+							<label x-text="card.name + ' of ' + card.group"></label>
+							<image :src="card.image_url" class="w-32 p-2">
 						</div>
                 	</template>
                 </div>
             </div>
+            <div class="flex flex-row flex-wrap justify-center p-4">
+                <button x-show="!gameIsOver" @click="playerHits();gameLogic()" @mouseover="hitHover = true" @mouseover.away="hitHover = false" :class="{'animate__animated animate__infinite animate__pulse relative bg-gray-800 text-white p-3 m-2 px-14 rounded text-xl font-bold overflow-hidden' : hitHover, 'relative bg-gray-800 text-white p-3 m-2 px-14 rounded text-xl font-bold overflow-hidden' : !hitHover}">Hit</button>
+                <button x-show="!gameIsOver" @click="playerSits();gameLogic()" @mouseover="foldHover = true" @mouseover.away="foldHover = false" :class="{'animate__animated animate__infinite animate__pulse relative bg-gray-800 text-white p-3 m-2 px-12 rounded text-xl font-bold overflow-hidden' : foldHover, 'relative bg-gray-800 text-white p-3 m-2 px-12 rounded text-xl font-bold overflow-hidden' : !foldHover}">Fold</button>
+				<button x-show="gameIsOver" @click="resetGame()" @mouseover="resetHover = true" @mouseover.away="resetHover = false" :class="{'animate__animated animate__infinite animate__pulse relative bg-gray-800 text-white p-3 m-2 px-12 rounded text-xl font-bold overflow-hidden' : resetHover, 'relative bg-gray-800 text-white p-3 m-2 px-12 rounded text-xl font-bold overflow-hidden' : !resetHover}">Play Again!</button>
+				
+			</div>
         </div>
     </div>
     <script>
     	function game() {
     		return {
     			rounds: 0,
+				gameIsOver: false,
 	    		dealerScore: 0,
 	    		playerScore: 0,
 	    		dealerCards: [],
 	    		playerCards: [],
 	    		playerHolds: false,
+	    		hitHover: false,
+	    		foldHover: false,
+				resetHover: false,
 	    		cards:[
 	    			@foreach($cards as $card)
 	    				{
@@ -48,14 +56,16 @@
 	    				},
 	    			@endforeach
 	    		],
-	    		gameLogic() {
+	    		async gameLogic() {
+	    			await pause(500);
 	    			if(this.dealerCanHit()) {
 	    				this.dealerHits();
 	    			}
 	    			this.calculatePlayerScore();
 	    			this.calculateDealerScore();
 	    			if(this.gameOver() || this.playerHolds) {
-	    				this.announceWinner();
+	    				this.gameIsOver = true;
+						this.announceWinner();
 	    			}
 	    			this.rounds++;
 	    		},
@@ -108,7 +118,8 @@
 	    			}
 	    			return true;
 	    		},
-	    		announceWinner() {
+	    		async announceWinner() {
+					await pause();
 	    			if(this.playerScore > this.dealerScore && this.playerScore <= 21) {
 	    				alert('Player wins!');
 	    			} else if(this.dealerScore > this.playerScore && this.dealerScore <= 21) {
@@ -120,8 +131,21 @@
 	    			} else if(this.dealerScore == this.playerScore) {
 	    				alert('It\'s a draw!');
 	    			}
-	    		}
+	    		},
+				resetGame() {
+					this.rounds = 0;
+					this.gameIsOver= false;
+					this.dealerScore = 0;
+					this.playerScore = 0;
+					this.dealerCards = [];
+					this.playerCards = [];
+					this.playerHolds = false;
+				}
 	    	}
     	}
+
+    	function pause(milliseconds = 1000) {
+            return new Promise(resolve => setTimeout(resolve, milliseconds));
+        }
     </script>
 </x-app-layout>
